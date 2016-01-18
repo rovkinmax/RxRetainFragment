b4c2e61e2f4a03000272e1cbbfeb0b526fccd637
Maxim Rovkin <rovkinmax@gmail.com>
2016-01-18 12:21:48 +0500

    move TestException in to separate class-file

--- app/src/test/java/com/github/rovkinmax/rxretainexample/SimpleActivityTest.kt
+++ app/src/test/java/com/github/rovkinmax/rxretainexample/SimpleActivityTest.kt
@@ -214,19 +214,3 @@ class SimpleActivityTest {
         secondSubscriber.assertCompleted()
     }
 }
-
-class TestException(detailMessage: String?) : RuntimeException(detailMessage) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other?.javaClass != javaClass) return false
-        if (other !is TestException) return false
-        if (!(message?.equals(other.message) ?: false)) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return message?.hashCode() ?: 0
-    }
-
-
-}
---
+++ app/src/test/java/com/github/rovkinmax/rxretainexample/TestException.kt
@@ -0,0 +1,18 @@
+package com.github.rovkinmax.rxretainexample
+
+/**
+ * @author Rovkin Max
+ */
+class TestException(detailMessage: String?) : RuntimeException(detailMessage) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != javaClass) return false
+        if (other !is TestException) return false
+        if (!(message?.equals(other.message) ?: false)) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return message?.hashCode() ?: 0
+    }
+}
