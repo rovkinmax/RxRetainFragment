9fad5bcf6ca31ed1b5c79ec9b4aaac3d093c24d0
Maxim Rovkin <rovkinmax@gmail.com>
2016-02-11 18:18:21 +0500

    add TestException  equals tests

---
+++ app/src/test/java/com/github/rovkinmax/rxretainexample/UntilTest.kt
@@ -0,0 +1,27 @@
+package com.github.rovkinmax.rxretainexample
+
+import com.github.rovkinmax.rxretainexample.test.TestException
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * @author Rovkin Max
+ */
+@RunWith(JUnit4::class)
+class UntilTest {
+    @Test
+    fun testEqualsTestException() {
+        val one = TestException("Expected")
+        val two = TestException("Expected")
+        Assert.assertEquals(one, two)
+    }
+
+    @Test
+    fun testNotEqualsTestException() {
+        val one = TestException("Expected")
+        val two = TestException("Unexpected")
+        Assert.assertNotEquals(one, two)
+    }
+}
